d85e6f563e6b40eb620954de52bcf854163f828c
Kaijian02 <130270626+Kaijian02@users.noreply.github.com>
2023-07-03 14:15:50 +0800

    Add files via upload

--- Jframe Software Development/src/Software_Development/Resident_Home.java
+++ Jframe Software Development/src/Software_Development/Resident_Home.java
@@ -26,7 +26,9 @@ public class Resident_Home extends javax.swing.JFrame {
         this.userId = userId;
         getContentPane().setBackground(new Color(51, 100, 173));
         initComponents();
-        
+        okBtn.setVisible(false);
+        reminder.setVisible(false);
+
         try {
             String url = "jdbc:mysql://localhost:3306/aps";
             String username = "root";
@@ -59,6 +61,35 @@ public class Resident_Home extends javax.swing.JFrame {
             e.printStackTrace();
         }
 
+        try {
+            Connection connection = null;
+            String url = "jdbc:mysql://localhost:3306/aps";
+            String username = "root";
+            String dbpassword = "";
+            connection = DriverManager.getConnection(url, username, dbpassword);
+            String reminderSql = "SELECT message FROM reminder WHERE user_id = ?";
+            PreparedStatement reminderStatement = connection.prepareStatement(reminderSql);
+            reminderStatement.setInt(1, userId);
+
+            ResultSet reminderResultSet = reminderStatement.executeQuery();
+
+            if (reminderResultSet.next()) {
+                // Retrieve the reminder message and display it on the home page
+                String reminderMessage = reminderResultSet.getString("message");
+                String fullMessage = "<html>Please pay the bills<br>" + reminderMessage.replace(",", ", ") + "</html>";
+                reminderLabel.setText(fullMessage);
+                okBtn.setVisible(true);
+                reminder.setVisible(true);
+            }
+
+            // Close the resources
+            reminderResultSet.close();
+            reminderStatement.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(this, "Error retrieving reminder: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+        }
+
     }
 
     /**
@@ -77,6 +108,10 @@ public class Resident_Home extends javax.swing.JFrame {
         testing = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
+        reminderPanel = new javax.swing.JPanel();
+        reminderLabel = new javax.swing.JLabel();
+        okBtn = new javax.swing.JButton();
+        reminder = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
         jButton4 = new javax.swing.JButton();
@@ -100,7 +135,7 @@ public class Resident_Home extends javax.swing.JFrame {
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel2Layout.createSequentialGroup()
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
                 .addContainerGap())
@@ -122,6 +157,46 @@ public class Resident_Home extends javax.swing.JFrame {
         jLabel5.setForeground(new java.awt.Color(0, 0, 0));
         jLabel5.setText("Name:");
 
+        okBtn.setText("OK");
+        okBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                okBtnActionPerformed(evt);
+            }
+        });
+
+        reminder.setForeground(new java.awt.Color(255, 51, 51));
+        reminder.setText("Reminder");
+
+        javax.swing.GroupLayout reminderPanelLayout = new javax.swing.GroupLayout(reminderPanel);
+        reminderPanel.setLayout(reminderPanelLayout);
+        reminderPanelLayout.setHorizontalGroup(
+            reminderPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, reminderPanelLayout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(reminderPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(reminder)
+                    .addGroup(reminderPanelLayout.createSequentialGroup()
+                        .addComponent(reminderLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 262, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(okBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap())
+        );
+        reminderPanelLayout.setVerticalGroup(
+            reminderPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(reminderPanelLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(reminder)
+                .addGroup(reminderPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(reminderPanelLayout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(reminderLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addContainerGap())
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, reminderPanelLayout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 19, Short.MAX_VALUE)
+                        .addComponent(okBtn)
+                        .addGap(38, 38, 38))))
+        );
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -130,14 +205,20 @@ public class Resident_Home extends javax.swing.JFrame {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(132, 132, 132)
-                        .addComponent(testing, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGap(132, 132, 132)
+                                .addComponent(testing, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGap(18, 18, 18)
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                    .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                    .addComponent(jLabel5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                        .addGap(0, 188, Short.MAX_VALUE))
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(18, 18, 18)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jLabel5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
-                .addContainerGap(194, Short.MAX_VALUE))
+                        .addContainerGap()
+                        .addComponent(reminderPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -148,9 +229,10 @@ public class Resident_Home extends javax.swing.JFrame {
                 .addComponent(jLabel5)
                 .addGap(26, 26, 26)
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(65, 65, 65)
-                .addComponent(testing)
-                .addContainerGap(29, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(reminderPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(testing))
         );
 
         jButton1.setText("Home");
@@ -161,8 +243,18 @@ public class Resident_Home extends javax.swing.JFrame {
         });
 
         jButton3.setText("Pay Bill");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
 
         jButton4.setText("Forum");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
 
         jButton5.setText("Profile");
         jButton5.addActionListener(new java.awt.event.ActionListener() {
@@ -230,7 +322,7 @@ public class Resident_Home extends javax.swing.JFrame {
                 .addGroup(layout.createSequentialGroup()
                     .addGap(56, 56, 56)
                     .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addContainerGap(72, Short.MAX_VALUE)))
+                    .addContainerGap(52, Short.MAX_VALUE)))
         );
 
         pack();
@@ -246,6 +338,48 @@ public class Resident_Home extends javax.swing.JFrame {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+        Resident_Bills obj = new Resident_Bills(userId);// obj created for class Second()
+        obj.setVisible(true); // Open the Second.java window
+        dispose(); // Close the First.java window
+    }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        // TODO add your handling code here:
+        Resident_Forum obj = new Resident_Forum(userId);// obj created for class Second()
+        obj.setVisible(true); // Open the Second.java window
+        dispose(); // Close the First.java window
+    }//GEN-LAST:event_jButton4ActionPerformed
+
+    private void okBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_okBtnActionPerformed
+        // TODO add your handling code here:
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/aps", "root", "")) {
+            // Prepare the SQL query to delete the reminder
+            String deleteSql = "DELETE FROM reminder WHERE user_id = ?";
+            PreparedStatement deleteStatement = connection.prepareStatement(deleteSql);
+            deleteStatement.setInt(1, userId); // Set the appropriate user_id
+
+            // Execute the query to delete the reminder
+            int rowsAffected = deleteStatement.executeUpdate();
+
+            if (rowsAffected > 0) {
+                reminderLabel.setText(""); // Clear the label after deleting the reminder
+                JOptionPane.showMessageDialog(this, "Please pay as soon as possible! Thank you", "Reminder", JOptionPane.INFORMATION_MESSAGE);
+                okBtn.setVisible(false);
+                reminder.setVisible(false);
+            } else {
+                JOptionPane.showMessageDialog(this, "Failed to delete the reminder.", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+
+            // Close the resources
+            deleteStatement.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            // Handle the error
+        }
+    }//GEN-LAST:event_okBtnActionPerformed
 
     /**
      * @param args the command line arguments
@@ -297,6 +431,10 @@ public class Resident_Home extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel5;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
+    private javax.swing.JButton okBtn;
+    private javax.swing.JLabel reminder;
+    private javax.swing.JLabel reminderLabel;
+    private javax.swing.JPanel reminderPanel;
     private javax.swing.JLabel testing;
     private javax.swing.JLabel unitNo;
     // End of variables declaration//GEN-END:variables
